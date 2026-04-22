853f698e656342c23d7ad7f63f8f87030ab0df60
Josué Alonzo-Chavarría <joshi.alonzo@gmail.com>
2026-04-22 12:49:33 -0600

    React Native Crash Course 2026: Checking The Platform

--- macrozone/src/app/index.tsx
+++ macrozone/src/app/index.tsx
@@ -1,9 +1,10 @@
-import { Text, View } from 'react-native';
+import { Platform, Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
     <View>
       <Text>Welcome to Macrozone!</Text>
+      <Text>Running on: {Platform.OS}</Text>
     </View>
   );
 }
