fd9d55cf342bce813e0d112edf1813a6666ec3fa
Josué Alonzo-Chavarría <joshi.alonzo@gmail.com>
2026-04-22 15:13:51 -0600

    React Native Crash Course 2026: Home Header With Date

--- macrozone/src/app/index.tsx
+++ macrozone/src/app/index.tsx
@@ -1,20 +1,12 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 import { globalStyles } from '@/styles/global'
+import HomeHeader from '@/components/HomeHeader';
 
 export default function HomeScreen() {
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.title}>MacroZone</Text>
-      <Text style={styles.date}>Monday, March 16</Text>
+      <HomeHeader />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  date: {
-    fontSize: 14,
-    color: '#a0a0b0',
-    marginTop: 4,
-    marginBottom: 30,
-  },
-});
