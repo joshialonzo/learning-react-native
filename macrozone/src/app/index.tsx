cf36428f86f51c574e3e8976156de6a91723e004
Josué Alonzo-Chavarría <joshi.alonzo@gmail.com>
2026-04-22 13:03:49 -0600

    React Native Crash Course 2026: Inline Styling

--- macrozone/src/app/index.tsx
+++ macrozone/src/app/index.tsx
@@ -1,14 +1,9 @@
-import * as Device from 'expo-device';
-import { Platform, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
 
 export default function HomeScreen() {
   return (
-    <View>
-      <Text>Welcome to Macrozone!</Text>
-      <Text>Running on: {Platform.OS}</Text>
-      <Text>Device Model: {Device.modelName}</Text>
-      <Text>Device Brand: {Device.brand}</Text>
-      <Text>OS Version: {Device.osVersion}</Text>
-    </View>
+   <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+    <Text>Hello World</Text>
+  </View>
   );
 }
